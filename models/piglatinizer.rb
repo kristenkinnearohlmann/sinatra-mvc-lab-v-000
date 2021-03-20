3271bfdc155e241612f15d0163521e71c05efbcf
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-20 12:54:16 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,7 +14,6 @@ class PigLatinizer
     words_arr.each do |word|
       split_word = word.split("")
       if @@vowels.include?(split_word[0].downcase)
-        binding.pry
         pig_latin << word + "way"
       else
         pig_latin << split_word[1..split_word.length()].join("") + split_word[0] + "ay"
