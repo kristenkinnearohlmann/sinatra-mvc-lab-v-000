397f6bd93e6663c05aad69a918e7bc1734376955
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-20 12:27:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/user_input' do
+
+    erb :user_input
+  end
+
+  post '/output' do
+    @input_phrase = params
+
+    erb :output    
+  end
+  
 end
