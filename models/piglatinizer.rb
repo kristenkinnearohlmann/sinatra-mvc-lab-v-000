3be94be538d0bf9953e770ea37d5905553c37695
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-20 12:48:24 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,8 +13,8 @@ class PigLatinizer
 
     words_arr.each do |word|
       split_word = word.split("")
+              binding.pry
       if @@vowels.include?(split_word[0])
-        binding.pry
         pig_latin << word
       else
         pig_latin << split_word[1..split_word.length()] + split_word[0]
